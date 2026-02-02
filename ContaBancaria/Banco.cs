9cc4251a78f12a195a8cb06f39f2485c3f2e2e60
Mariany Fonseca <marianyf76@gmail.com>
2026-02-02 01:09:00 -0300

    Add files via upload

---
+++ ContaBancaria/Banco.cs
@@ -0,0 +1,38 @@
+﻿using System.Globalization;
+
+namespace ContaBancaria
+{
+    internal class Banco
+    {
+        public double Saldo { get; private set; }
+        public int NumeroDaConta { get; private set; }
+        public string NomeDoTitular { get; set; }
+
+        public Banco(int numeroDaConta, string nomeDoTitular)
+        {
+            NumeroDaConta = numeroDaConta;
+            NomeDoTitular = nomeDoTitular;
+        }
+
+        public Banco(int numeroDaConta, string nomeDoTitular, double saldo) : this(numeroDaConta, nomeDoTitular)
+        {
+            Saldo = saldo;
+        }
+
+        public void Sacar(double valor)
+        {
+            Saldo = Saldo - valor - 5.00;   
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public override string ToString()
+        {
+            return $"Número da conta: {NumeroDaConta}, Nome do Titular: {NomeDoTitular}, Saldo: R$" +
+                $" {Saldo.ToString("F2",CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Runtime.Remoting;
+
+namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco cliente;
+            Console.WriteLine("Informe o nome do titular:");
+            string nomeTitular = Console.ReadLine();
+            Console.WriteLine("Informe o número da conta:");
+            int numeroDaConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Deseja realizar o primeiro deposito:");
+            Console.WriteLine("1 - Sim");
+            Console.WriteLine("2 - Não");
+            int realizarDeposito = int.Parse(Console.ReadLine());
+            if (realizarDeposito == 1)
+            {
+                Console.WriteLine("Quanto deseja depositar:");
+                double primeiroDeposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                cliente = new Banco(numeroDaConta, nomeTitular, primeiroDeposito);
+            }
+            else
+            {
+                cliente = new Banco(numeroDaConta, nomeTitular);
+            }
+            Console.WriteLine(cliente);
+            Console.WriteLine("Realizar deposito:");
+            Console.WriteLine("Quanto deseja depositar:");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente.Depositar(deposito);
+            Console.WriteLine(cliente);
+            Console.WriteLine("Quanto deseja sacar:");
+            double sacar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            cliente.Sacar(sacar);
+            Console.WriteLine(cliente);
+
+
+        }
+    }
+}
