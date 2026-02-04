c841c1adaabf4694c9039dccd8a15a3c04937dd0
Mariany Fonseca <marianyf76@gmail.com>
2026-02-04 11:27:02 -0300

    Add files via upload

---
+++ Aluguel/Hotel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste7
+{
+    internal class Hotel
+    {
+        public Hotel()
+        {
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Hotel(string name, string email)
+        {
+            Name = name;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome = {Name}, Email = {Email}";
+        }
+    }
+}
---
+++ Aluguel/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace Teste7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel[] room = new Hotel[10];
+            Console.WriteLine("Quantos quartos serão alugados:");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++){
+                Console.WriteLine("Aluguel #" +  i);
+                Console.WriteLine("Qual o seu nome:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Informe seu Email:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Qual quarto deseja:");
+                int num = int.Parse(Console.ReadLine());
+                room[num] = new Hotel(name, email);
+            }
+            Console.WriteLine("Quartos Alugados:");
+            for (int i = 1; i < 10; i++)
+            {
+                if (room[i] != null)
+                {
+                    Console.WriteLine("Quarto " + i + ": " + room[i].ToString());
+                }
+
+            }
+
+            
+        }
+    }
+}
