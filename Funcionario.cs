af2b5437d2ed4d88278585ed459cbd5b75af8148
Mariany Fonseca <marianyf76@gmail.com>
2026-02-10 10:43:25 -0300

    Add files via upload
    Este projeto é um sistema de gerenciamento de funcionários desenvolvido em C#, com foco em praticar Programação Orientada a Objetos, listas e validação de dados.
    
    Funcionalidades incluem:
    Cadastro de funcionários
    Armazenamento em listas
    Busca por ID
    Atualização de salário por porcentagem
    Validação de entradas do usuário para evitar erros de conversão
    
    O objetivo principal é consolidar conceitos como classes, métodos, construtores, listas, expressões lambda e boas práticas de organização de código.

---
+++ Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Registrar
+{
+    internal class Funcionario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+        public void AumentarSalario(double n)
+        {
+            Salario += Salario * n / 100.00;
+        }
+        public override string ToString()
+        {
+            return $"{Id} , {Nome} , {Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+            ;
+        }
+    }
+}
---
+++ InputValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Registrar
+{
+    static class InputValidator
+    {
+        public static double ValidarDouble(string menssagem)
+        {
+            double num;
+            Console.WriteLine(menssagem);
+            while (!double.TryParse(Console.ReadLine(),NumberStyles.Any, CultureInfo.InvariantCulture, out num))
+            {
+                Console.WriteLine("Inválido, Tente novamente!");
+            }
+            return num;
+        }
+        public static int ValidarInt(string menssagem)
+        {
+            int num;
+            Console.WriteLine(menssagem);
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Inválido, Tente novamente!");
+            }
+            return num;
+        }
+    }   
+}
