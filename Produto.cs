cff8b36493b98589e4cd4263ad2be06c1d126d19
Mariany Fonseca <marianyf76@gmail.com>
2026-02-10 10:35:25 -0300

    Add files via upload
    Este projeto é um sistema de gerenciamento de funcionários desenvolvido em C#, com foco em praticar Programação Orientada a Objetos, listas e validação de dados.
    
    Funcionalidades incluem:
    Cadastro de funcionários
    Armazenamento em listas
    Busca por ID
    Atualização de salário por porcentagem
    Validação de entradas do usuário para evitar erros de conversão
    
    O objetivo principal é consolidar conceitos como classes, métodos, construtores, listas, expressões lambda e boas práticas de organização de código.

---
+++ Produto.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Globalization;
+
+namespace SistemaDeCadastro_
+{
+    internal class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+
+
+        }
+
+        public double ValorTotal()
+        {
+            return Preco * Quantidade;
+        }
+        public static int LerInt(string mensagem)
+        {
+            int valor;
+            Console.WriteLine(mensagem);
+            while (!int.TryParse(Console.ReadLine(), out valor))
+            {
+                Console.WriteLine("Valor inválido, tente novamente:");
+            }
+            return valor;
+        }
+        public static double LerDouble(string mensagem)
+        {
+            double valor;
+            Console.WriteLine(mensagem);
+            while (!double.TryParse(Console.ReadLine(), NumberStyles.Any, CultureInfo.InvariantCulture, out valor))
+            {
+                Console.WriteLine("Valor inválido, tente novamente:");
+            }
+            return valor;
+        }
+
+        public static int LerPosicao(Produto[] vetor)
+        {
+            int posicao;
+            while (true)
+            {
+                posicao = LerInt("Digite a posição");
+                if (posicao < 0 || posicao >= vetor.Length)
+                {
+                    Console.WriteLine("Posição inválida, tente novamente");
+                }
+                else if (vetor[posicao] != null)
+                {
+                    Console.WriteLine("Posição ocupada, tente novamente");
+                }
+                else
+                {
+                    return posicao;
+                }
+
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Nome = {Nome}, Preço = {Preco.ToString("F2", CultureInfo.InvariantCulture)}, Quantidade = {Quantidade}";
+        }
+    }
+}
